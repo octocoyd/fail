eacd7381c70e96fd766270503d44a0ea9c9b6f45
octocoyd <112360657+octocoyd@users.noreply.github.com>
2023-03-20 18:15:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Your index.js file is loaded correctly!");
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
